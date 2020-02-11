1d9dcac507313d45904edee1a9bd1bd9415f57ac
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-02-11 10:56:03 -0300

    Resetando senha

--- goadonis/app/Controllers/Http/ForgotPasswordController.js
+++ goadonis/app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const User = use('App/Models/User')
 const crypto = require('crypto')
 const Mail = use('Mail')
@@ -32,6 +33,25 @@ class ForgotPasswordController {
           message: 'Something is wrong. This email exists?'
         }
       })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment().subtract('2', 'days').isAfter(user.token_created_at)
+      if(tokenExpired) {
+        return response.status(401).send({error: {message: "Token Expired"}})
+      }
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+      await user.save()
+    } catch (err) {
+      return response
+            .status(err.status)
+            .send({ error:{ message: "Reset password do not works." } })
     }
   }
 }
