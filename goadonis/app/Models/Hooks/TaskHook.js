51c8887f037a0cf8e38731aa842a7a47eaa17f7c
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-02-17 12:17:03 -0300

    fila com Redis

--- goadonis/app/Models/Hooks/TaskHook.js
+++ goadonis/app/Models/Hooks/TaskHook.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
+
 const TaskHook = exports = module.exports = {}
 
 TaskHook.sendNewTaskMail = async (taskInstance) => {
@@ -12,20 +13,7 @@ TaskHook.sendNewTaskMail = async (taskInstance) => {
   const { email, username } = await taskInstance.user().fetch()
   const file = await taskInstance.file().fetch()
   
-  await Mail.send(
-    ['emails.new_task'], {
-      username, title, hasAttachment:!!file
-    },
-    message => {
-      message.to(email).from('matheuspsasntos@gmail.com', 'Matheus')
-              .subject('Nova tarefa para voce')
+  const {title} = taskInstance
 
-      if(file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
-
+  Kue.dispatch(Job.key, { email, username, title, file }, { attempts: 3 })
 }
